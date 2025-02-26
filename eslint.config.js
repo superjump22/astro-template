d3e46dd73b733de643f5d65951c81a27206f1448
Super Jump <superjump.void@gmail.com>
2025-02-26 20:21:56 +0800

    init

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  lessOpinionated: true,
+  astro: true,
+  react: true,
+  vue: true,
+  stylistic: true,
+  typescript: true,
+  formatters: true,
+  ignores: ['**/.astro/**', '**/public/**', '**/assets/**'],
+})
