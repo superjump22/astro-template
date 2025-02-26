55fe6fe53d268ecc35cdac3202b52020374468cf
Super Jump <superjump.void@gmail.com>
2025-02-26 20:34:52 +0800

    add deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import react from '@astrojs/react'
+import vue from '@astrojs/vue'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), vue()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+})
