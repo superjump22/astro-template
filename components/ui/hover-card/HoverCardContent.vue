5078c0ee8c8b481078f38b69d54f5c094ed4c6ca
Super Jump <superjump.void@gmail.com>
2025-05-17 12:20:01 +0800

    shadcn-vue

--- components/ui/hover-card/HoverCardContent.vue
+++ components/ui/hover-card/HoverCardContent.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils'
+import type { HoverCardContentProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
 import {
   HoverCardContent,
-  type HoverCardContentProps,
+
   HoverCardPortal,
   useForwardProps,
 } from 'reka-ui'
-import { computed, type HTMLAttributes } from 'vue'
+import { computed } from 'vue'
+import { cn } from '@/lib/utils'
 
 const props = withDefaults(
   defineProps<HoverCardContentProps & { class?: HTMLAttributes['class'] }>(),
